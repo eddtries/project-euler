b88dcf56c1c9239615e1ca57cfcaa64a15ec4b88
eddtries <eddtries@icloud.com>
2023-10-28 10:31:50 +0200

    20 is the same as 16 lol

--- pkg/solutions/problem_0020.go
+++ pkg/solutions/problem_0020.go
@@ -7,9 +7,14 @@ import (
 
 // https://projecteuler.net/problem=16
 
-func Problem0016() int {
-	number := new(big.Int)
-	number.Exp(big.NewInt(2), big.NewInt(1000), nil)
+func Problem0020() int {
+	number := big.NewInt(1)
+
+	for i := 100; i > 0; i-- {
+		iAsBigNum := big.NewInt(int64(i))
+		number.Mul(number, iAsBigNum)
+	}
+
 	numberAsString := number.Text(10)
 	result := 0
 	for i := 0; i < len(numberAsString); i++ {
